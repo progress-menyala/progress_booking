759ddcc7cf4300a4f40a52a506beca75f9c5f476
Tio Mahesa <tiomahesa.work@gmail.com>
2024-07-09 23:06:03 +0800

    Update || Upgrade Laravel version to 11 & Setup Filament & Crud User

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/sass/app.scss', // Our new line you can change app.scss to whatever.scss
-                'resources/js/app.js',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
