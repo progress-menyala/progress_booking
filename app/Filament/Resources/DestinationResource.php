8f56eeb23098ff3e0aec159201e6c57617b3ed2f
Tio Mahesa <tiomahesa.work@gmail.com>
2024-07-17 01:32:13 +0800

    Add || CRUD destination

---
+++ app/Filament/Resources/DestinationResource.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\DestinationResource\Pages;
+use App\Filament\Resources\DestinationResource\RelationManagers;
+use App\Models\Destination;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class DestinationResource extends Resource
+{
+    protected static ?string $model = Destination::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('user_id')
+                    ->relationship('user', 'id')
+                    ->relationship('user', 'name')
+                    ->required()
+                    ,
+
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('location')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('capacity')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\Textarea::make('description')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\FileUpload::make('images')
+                    ->label('Images')
+                    ->multiple()
+                    ->directory('uploads/images')
+                    ->image()
+                    ->maxSize(2048),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('user_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('location')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('capacity')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ])
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListDestinations::route('/'),
+            'create' => Pages\CreateDestination::route('/create'),
+            'edit' => Pages\EditDestination::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/Destination.php
+++ app/Models/Destination.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Destination extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    // public function registerMediaCollections(): void
+    // {
+    //     $this->addMediaCollection('images')->useDisk('public')->singleFile();
+    // }
+
+    protected $casts = [
+        'images' => 'array',
+    ];
+
+    public function user()
+    {
+        return $this->hasMany(User::class);
+    }
 }
